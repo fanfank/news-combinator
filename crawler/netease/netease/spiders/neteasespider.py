4d7699f38d3b8d586560628788244a86f230ed5f
fanfank <reetsee.xu@gmail.com>
2014-01-24 20:14:15 +0800

    Finished 163 news spider demo

--- crawler/netease/netease/spiders/neteasespider.py
+++ crawler/netease/netease/spiders/neteasespider.py
@@ -4,27 +4,28 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 
-from tencent.items import TencentItem
-from tencent.news_pack.news_func import ListCombiner
+from netease.items import NeteaseItem
+from netease.news_pack.news_func import ListCombiner
 
-class TencentNewsSpider(CrawlSpider):
-    name = 'tencent_news_spider'
-    allowed_domains = ['news.qq.com']
-    start_urls = ['http://news.qq.com']
-    url_pattern = r'(.*)/a/(\d{8})/(\d+)\.htm'
+class NeteaseNewsSpider(CrawlSpider):
+    name = 'netease_news_spider'
+    allowed_domains = ['news.163.com']
+    start_urls = ['http://news.163.com']
+    url_pattern = r'(http://news\.163\.com)/(\d{2})/(\d{4})/\d+/(\w+)\.html'
     rules = [Rule(SgmlLinkExtractor(allow=[url_pattern]), 'parse_news')]
-    
+
     def parse_news(self, response):
         sel = Selector(response)
         pattern = re.match(self.url_pattern, str(response.url))
-        item = TencentItem()
+
+        item = NeteaseItem()
         item['source'] = pattern.group(1)
-        item['date'] = pattern.group(2)
-        item['newsId'] = pattern.group(3)
-        item['cmtId'] = sel.re(r"cmt_id = (.*)")[0] # unicode string
-        item['comments'] = {'link':str('http://coral.qq.com/')+item['cmtId']}
+        item['date'] = '20' + pattern.group(2) + pattern.group(3)
+        item['newsId'] = pattern.group(4)
+        item['cmtId'] = item['newsId']
+        item['boardId'] = sel.re(r"boardId = \"(.*)\"")[0]
+        item['comments'] = {'link':str('http://comment.news.163.com/'+item['boardId']+'/'+item['cmtId']+'.html')}
         item['contents'] = {'link':str(response.url), 'title':u'', 'passage':u''}
         item['contents']['title'] = sel.xpath('//h1/text()').extract()[0]
         item['contents']['passage'] = ListCombiner(sel.xpath('//p/text()').extract())
         return item
-
