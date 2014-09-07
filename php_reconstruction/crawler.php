c32570878a82f0b99549999983a53f514fd2f62a
fanfank <reetsee@qq.com>
2014-09-07 21:22:29 +0800

    Finished crawler by xuruiqi

--- php_reconstruction/crawler.php
+++ php_reconstruction/crawler.php
@@ -3,6 +3,168 @@ require_once('phpfetcher.php');
 class news_crawler extends Phpfetcher_Crawler_Default {
     public function handlePage($page) {
         //print_r($page->getHyperLinks());
+        $arrExtraInfo = $page->getExtraInfo(array('job_name'));
+        $strJobName = $arrExtraInfo['job_name'];
+
+        switch ($strJobName) {
+            case 'tencent':
+                $arrData = $this->parseTencentNews($page);
+                break;
+            case 'netease':
+                $arrData = $this->parseNeteaseNews($page);
+                break;
+            case 'sina':
+                $arrData = $this->parseSinaNews($page);
+                break;
+        }
+    }
+
+    /**
+     * @author xuruiqi
+     * @desc 解析腾讯新闻
+     */
+    public function parseTencentNews($page) {
+        $strUrl = $page->getUrl();
+        $timestamp = intval(time());
+
+        $arrContent = array();
+        $objContent = $page->xpath('//p/text()');
+        for ($i = 0; $i < $objContent->length; ++$i) {
+            $arrContent[] = $objContent->item($i)->nodeValue;
+        }
+
+        $matches = array();
+        preg_match('/(.*)\/a\/(\d{8})\/(\d+)\.htm/', $strUrl, $matches);
+
+        //获取评论id
+        preg_match('/cmt_id = (.*);/', $page->getContent(), $matches_comment_id);
+
+        $arrOutput = array(
+            'news_abstract' => array(
+                'id'          => 0,
+                'title'       => $page->xpath('//h1/text()')->item(0)->nodeValue,
+                'icon_pic'    => '',
+                'rate_points' => 0,
+                'rate_counts' => 0,
+                'quality'     => 0,
+                'content_id'  => 0,
+                'day_time'    => intval($matches[2]),
+                'timestamp'   => $timestamp,
+            ),
+            'news_content' => array(
+                'id'                  => 0, 
+                'source_name'         => 'tencent',
+                'content'             => serialize($arrContent),
+                'source_news_link'    => $strUrl,
+                'source_comment_link' => "http://coral.qq.com/{$matches_comment_id[1]}",
+                'source_news_id'      => strval($matches[3]),
+                'source_comment_id'   => strval($matches_comment_id[1]),
+                'abstract_id'         => 0,
+                'timestamp'           => $timestamp,
+                'ext'                 => '',
+            ),
+        );
+        return $arrOutput;
+    }
+
+    /**
+     * @author xuruiqi
+     * @desc 解析网易新闻
+     */
+    public function parseNeteaseNews($page) {
+        $strUrl = $page->getUrl();
+        $timestamp = intval(time());
+
+        $arrContent = array();
+        $objContent = $page->xpath('//p/text()');
+        for ($i = 0; $i < $objContent->length; ++$i) {
+            $arrContent[] = $objContent->item($i)->nodeValue;
+        }
+
+        $matches = array();
+        preg_match('/(http://news\.163\.com)/(\d{2})/(\d{4})/\d+/(\w+)\.html/', $strUrl, $matches);
+
+        //获取boardId
+        preg_match('/boardId = "(.*)"/', $page->getContent(), $matches_board_id);
+
+        $arrOutput = array(
+            'news_abstract' => array(
+                'id'          => 0,
+                'title'       => $page->xpath('//h1[@id=\'h1title]\'/text()')->item(0)->nodeValue,
+                'icon_pic'    => '',
+                'rate_points' => 0,
+                'rate_counts' => 0,
+                'quality'     => 0,
+                'content_id'  => 0,
+                'day_time'    => intval("20{$matches[2]}{$matches[3]}"),
+                'timestamp'   => $timestamp,
+            ),
+            'news_content' => array(
+                'id'                  => 0, 
+                'source_name'         => 'netease',
+                'content'             => serialize($arrContent),
+                'source_news_link'    => $strUrl,
+                'source_comment_link' => "http://comment.news.163.com/{$matches_board_id[1]}/{$matches[4]}.html",
+                'source_news_id'      => strval($matches[4]),
+                'source_comment_id'   => strval($matches[4]),
+                'abstract_id'         => 0,
+                'timestamp'           => $timestamp,
+                'ext'                 => serialize(array('board_id' => $matches_board_id[1])),
+            ),
+        );
+        return $arrOutput;
+    }
+
+    /**
+     * @author xuruiqi
+     * @desc 解析新浪新闻
+     */
+    public function parseSinaNews($page) {
+        $strUrl = $page->getUrl();
+        $timestamp = intval(time());
+
+        //获取新闻正文
+        $arrContent = array();
+        $objContent = $page->xpath('//p/text()');
+        for ($i = 0; $i < $objContent->length; ++$i) {
+            $arrContent[] = $objContent->item($i)->nodeValue;
+        }
+
+        $matches = array();
+        preg_match('/(http://(?:\w+\.)*news\.sina\.com\.cn)/.*/(\d{4}-\d{2}-\d{2})/\d{4}(\d{8})\.(?:s)html/', $strUrl, $matches);
+
+        //获取newsId
+        preg_match('/comment_id:(\d-\d-\d+)/', $page->getContent(), $matches_news_id)
+
+        //获取channelId
+        preg_match('/comment_channel:(\w+);/', $page->getContent(), $matches_channel_id);
+
+        $arrOutput = array(
+            'news_abstract' => array(
+                'id'          => 0,
+                'title'       => $page->xpath('//h1[@id=\'artibodyTitle\']/text()')->item(0)->nodeValue,
+                'icon_pic'    => '',
+                'rate_points' => 0,
+                'rate_counts' => 0,
+                'quality'     => 0,
+                'content_id'  => 0,
+                'day_time'    => intval(implode(explode('-', $matches[2]))),
+                'timestamp'   => $timestamp,
+            ),
+            'news_content' => array(
+                'id'                  => 0, 
+                'source_name'         => 'sina',
+                'content'             => serialize($arrContent),
+                'source_news_link'    => $strUrl,
+                'source_comment_link' => "http://comment5.news.sina.com.cn/comment/skin/default.html?channel={$matches_channel_id[1]}&newsid={$matches_news_id[1]}",
+                'source_news_id'      => $matches_news_id[1],
+                'source_comment_id'   => $matches_news_id[1],
+                'abstract_id'         => 0,
+                'timestamp'           => $timestamp,
+                'ext'                 => serialize(array('channel_id' => $matches_channel_id[1])),
+            ),
+        );
+        return $arrOutput;
     }
 }
 
@@ -18,7 +180,7 @@ $arrFetchJobs = array(
     'netease' => array(
         'start_page' => 'http://news.163.com', 
         'link_rules' => array(
-            '/(http://news\.163\.com)/(\d{2})/(\d{4})/\d+/(\w+)\.html/',   
+            '/(http://news\.163\.com)/(\d{2})/(\d{4})/\d+/(\w+)\.html/', 
         ),
         'max_depth' => 4, 
     ),        
