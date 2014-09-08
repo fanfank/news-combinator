a1971efee0c9288bdf4e95d1985b26242d8514a2
fanfank <reetsee@qq.com>
2014-09-08 21:51:19 +0800

    Finished sql_functions

--- php_reconstruction/sql_functions.php
+++ php_reconstruction/sql_functions.php
@@ -4,8 +4,9 @@ class Reetsee_Sql {
     const SQL_PART_FROM      = 2;
     const SQL_PART_WHERE_AND = 3;
     const SQL_PART_UPDATE    = 4;
-    const SQL_PART_SET       = 5;
-    
+    const SQL_PART_VALUES    = 5; 
+    const SQL_PART_DUP       = 6;
+
     public static function getSqlDelete($table, $conds, $arrExtra) {
         $arrSql   = array();
         $arrSql[] = self::_getSqlPart($table, self::SQL_PART_DEL);
@@ -29,6 +30,94 @@ class Reetsee_Sql {
             }
         }
         return implode(' ', $arrSql);
+    }
+
+    protected static function _getSqlPart($tuples, $tuples_type) {
+        $sql = '';
+        switch ($tuples_type) {
+            case self::SQL_PART_SEL: //SELECT 模板
+                if (!is_array($tuples) || empty($tuples)) {
+                    $sql = FALSE;
+                    break;
+                }
+
+                $sql .= "SELECT ";
+                $arrFields = array();
+                foreach ($tuples as $field) {
+                    $field = mysql_escape_string($field);
+                    $arrFields[] = $field;
+                }
+                $sql .= implode(', ', $arrFields);
+                break;
+
+            case self::SQL_PART_FROM: //FROM 模板
+                if (!is_string($tuples) || 0 === strlen($tuples)) {
+                    $sql = FALSE;
+                    break;
+                }
+
+                $tuples = mysql_escape_string($tuples);
+                $sql .= "FROM `$tuples`";
+                break;
+
+            case self::SQL_PART_WHERE_AND: //WHERE AND 模板
+                if (!is_array($tuples) || empty($tuples)) {
+                    $sql = FALSE;
+                    break;
+                }
+
+                $sql .= 'WHERE ';
+                $arrAndConds = array();
+                foreach ($tuples as $field => $value) {
+                    $field = mysql_escape_string($field);
+                    $value = mysql_escape_string($value);
+                    $arrAndConds[] = "`$field`=\'$value\'";
+                }
+                $sql .= implode(' AND ', $arrAndConds);
+                break;
+
+            case self::SQL_PART_UPDATE: //UPDATE 模板
+                if (!is_string($tuples) || 0 === strlen($tuples)) {
+                    $sql = FALSE;
+                    break;
+                }
+
+                $tuples = mysql_escape_string($tuples);
+                $sql .= "UPDATE `$tuples` SET";
+                break;
+
+            case self::SQL_PART_VALUES: //KEY=VALUE 模板
+                if (!is_array($tuples) || empty($tuples)) {
+                    $sql = FALSE;
+                    break;
+                }
+
+                $sql .= "";
+                $arrKeyValues = array();
+                foreach ($tuples as $field => $value) {
+                    $field = mysql_escape_string($field);
+                    $value = mysql_escape_string($value);
+                    $arrKeyValues[] = "`$field`=\'$value\'";
+                }
+                $sql .= implode(', ', $arrKeyValues);
+                break;
+
+            case self::SQL_PART_DUP: //ON DUPLICATE 模板
+                $values_sql = self::_getSqlPart($tuples, self::SQL_PART_VALUES);
+                if (FALSE === $values_sql) {
+                    $sql = FALSE;
+                    break;
+                }
+
+                $sql = "ON DUPLICATE KEY UPDATE $values_sql";
+                break;
+
+            default:
+                $sql = FALSE;
+                break;
+
+        }
+        return $sql;
     }
 
     public static function getSqlSelect($fields, $table, $conds, $arrExtra) {
