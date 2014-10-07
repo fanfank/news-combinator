219fd3f747403f18a283e9b6200e82f2edf6c908
fanfank <reetsee@qq.com>
2014-10-07 12:58:39 +0800

    updated con_mysql

--- reconstruction/category/test/con_mysql.c
+++ reconstruction/category/test/con_mysql.c
@@ -3,7 +3,7 @@ int main(int argc, char *argv[]) {
     MYSQL mysql;
     mysql_init(&mysql);
     //mysql_options(&mysql, MYSQL_READ_DEFAULT_GROUP, "reetsee_news");
-    mysql_options(&mysql, MYSQL_SET_CHARSET_NAME, "utf8");
+    //mysql_options(&mysql, MYSQL_SET_CHARSET_NAME, "utf8");
     if (!mysql_real_connect(&mysql, "127.0.0.1", "reetsee", "123abc", "reetsee_news", 3306, NULL, 0)) {
         fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(&mysql));
         exit(1);
@@ -11,6 +11,10 @@ int main(int argc, char *argv[]) {
         /*
          * success
          */
+    }
+
+    if (!mysql_set_character_set(&mysql, "utf8")) {
+        printf("New client character set: %s\n", mysql_character_set_name(&mysql));
     }
 
     mysql_close(&mysql);
