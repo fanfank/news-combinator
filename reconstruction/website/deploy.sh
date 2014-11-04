4435f6f02b981a3fca0256e16a5f326958dd18e6
fanfank <reetsee@qq.com>
2014-11-04 22:05:53 +0800

    Modified deploy.sh

--- reconstruction/website/deploy.sh
+++ reconstruction/website/deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-DEPLOY_PATH="$ENV_PATH/php/phplib"
+DEPLOY_PATH="$ENV_PATH/module/news"
 
 OUTPUT_DIR="output"
-PRODUCT="phpfetcher"
+PRODUCT="news"
 OUTPUT_FILE="$PRODUCT.tar.gz"
 
 mkdir -p $OUTPUT_DIR
 rm -rf $OUTPUT_DIR/*
-cp -rf Phpfetcher phpfetcher.php $OUTPUT_DIR/
+cp -rf html actions index.php $OUTPUT_DIR/
 
 cd $OUTPUT_DIR
 find ./ -name .git -exec rm -rf {} \;
 tar zcvf $OUTPUT_FILE ./*
 
-rm -rf Phpfetcher phpfetcher.php
+rm -rf html actions index.php
 
 cp $OUTPUT_FILE $DEPLOY_PATH/
 cd $DEPLOY_PATH
